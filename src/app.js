38a9bb0cea351db203823b807b50824a5f6fab05
devreinert <gui.moop7@gmail.com>
2026-04-12 21:37:33 -0300

    Finalizando aula 5, filtrando selecoes por id e excluido selecoes por id

--- src/app.js
+++ src/app.js
@@ -9,6 +9,16 @@ const selecoes = [
     {id: 4, selecao: 'Argentina', grupo: 'G' },
 ]
 
+//pegar o objeto por id
+function buscarSelecaoPorId(id){
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+//pegar posição no array
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
+
 //criar uma rota padrao ou raiz
 
 app.get('/', (req, res) =>{ 
@@ -20,8 +30,18 @@ app.get('/selecoes', (req, res) =>{
     res.status(200).send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req,res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada!')
+})
+
+app.delete('/selecoes/:id', (req, res) =>{
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`Seleção com o id ${req.params.id} excluida com sucesso!`)
 })
 export default app
